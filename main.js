b8ad9e8ced9eee928afa6ca05a6e01a84b5e1916
Camille <camillegomes74@gmail.com>
2021-09-03 22:17:33 -0300

    Feat: Random Image Position Function

--- main.js
+++ main.js
@@ -9,3 +9,25 @@ function ajustaTamanhoPalco(){
 }
 
 ajustaTamanhoPalco()
+
+// CRIANDO POSIÇÕES RANDÔMICAS
+function posicaoRandomica(){
+    var posicaoX = Math.floor(Math.random() * largura) - 90
+    var posicaoY = Math.floor(Math.random() * altura) - 90
+
+    // Impede a criação de posições negativas aleatórias 
+    posicaoX = posicaoX < 0 ? 0 : posicaoX
+    posicaoY = posicaoY < 0 ? 0 : posicaoY
+
+    // Criando elemento HTML
+    var mosquito = document.createElement('img')
+    mosquito.src = "imagens/mosquito.png"
+    mosquito.className = 'mosquito1'
+    mosquito.style.left = posicaoX + 'px'
+    mosquito.style.top = posicaoY + 'px'
+    mosquito.style.position = 'absolute'
+
+    document.body.appendChild(mosquito)
+}
+
+posicaoRandomica()
