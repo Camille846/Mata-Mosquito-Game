3d034d980e4e3991e275c87c6dfb8526eb2f28a7
Camille <camillegomes74@gmail.com>
2021-09-04 09:26:44 -0300

    Feat: random mosquitos size function and classes

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ function posicaoRandomica(){
     // Criando elemento HTML
     var mosquito = document.createElement('img')
     mosquito.src = "imagens/mosquito.png"
-    mosquito.className = 'mosquito1'
+    mosquito.className = tamanhoAleatorio()
     mosquito.style.left = posicaoX + 'px'
     mosquito.style.top = posicaoY + 'px'
     mosquito.style.position = 'absolute'
@@ -31,3 +31,18 @@ function posicaoRandomica(){
 }
 
 posicaoRandomica()
+
+// CRIANDO TAMANHOS RANDÔMICOS 
+function tamanhoAleatorio(){
+    var classe = Math.floor(Math.random() * 3)
+
+    switch(classe){
+        case 0:
+            return 'mosquito1'
+        case 1:
+            return 'mosquito2'
+        case 2:
+            return 'mosquito3'
+        
+    }
+}
